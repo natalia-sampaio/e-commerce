02e4359160308cf0b1ce956a092ca7bd5f1352d4
Natália Sampaio Rosa e Silva <natalia.srs@outlook.com>
2023-04-13 15:50:43 -0300

    :bug: fix: mobile navbar animation broke

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@ import IconMenu from './components/icons/IconMenu.vue';
 import { useCartStore } from './stores/cart';
 import Cart from './components/Cart.vue';
 import { Transition } from 'vue';
-import MobileNav from './components/MobileNav.vue';
-import DesktopNav from './components/DesktopNav.vue';
+import NavLinks from './components/NavLinks.vue';
+import IconClose from './components/icons/IconClose.vue';
 
 const cartStore = useCartStore();
 </script>
@@ -46,9 +46,43 @@ export default {
     <div class="flex items-center transition-all duration-500">
       <IconMenu class="m-3 transition-all duration-700 motion-reduce:transition-all xl:hidden" :class="{'rotate-180': mobileNav}" @click="toggleMobileNav" />
       <RouterLink :to="{name: 'home'}"><IconLogo /></RouterLink>
-      <DesktopNav v-if="desktop" />
+      <div v-if="desktop" class="w-full">
+        <NavLinks />
+      </div>
       <Transition name="mobile-nav">
-        <MobileNav v-show="mobileNav" :toggleMobileNav="toggleMobileNav" :mobile-nav="mobileNav"/>
+        <div v-show="mobileNav" 
+        class="
+          bg-white 
+          fixed xl:relative 
+          top-0 left-0 
+          h-full xl:h-auto 
+          w-2/3 xl:w-full 
+          z-30 xl:z-0 ">
+          <IconClose class="m-5 absolute right-0 transition-all duration-700 motion-reduce:transition-all xl:hidden" @click="toggleMobileNav" :class="{ '-rotate-180': !mobileNav }" />
+          <ul class="mx-5 my-10 xl:text-blue-dark-grayish xl:font-normal xl:flex xl:justify-evenly">
+              <li class="my-5 xl:ml-10">
+                  <RouterLink :to="{ name: 'collections' }" @click="mobileNav = false">Collections</RouterLink>
+              </li>
+              <li class="my-5 xl:ml-10">
+                  <RouterLink :to="{ name: 'men' }" @click="mobileNav = false">Men</RouterLink>
+              </li>
+              <li class="my-5 xl:ml-10">
+                  <RouterLink :to="{ name: 'women' }" @click="mobileNav = false">Women</RouterLink>
+              </li>
+              <li class="my-5 xl:ml-10">
+                  <RouterLink :to="{ name: 'about' }" @click="mobileNav = false">About</RouterLink >
+              </li>
+              <li class="my-5 xl:ml-10">
+                  <RouterLink :to="{ name: 'contact' }" @click="mobileNav = false">Contact</RouterLink>
+              </li>
+              <li class="my-5 xl:ml-10 border-b border-blue-dark-grayish pb-5">
+                  <button>Sign in</button>
+              </li>
+              <li class="my-5 xl:ml-10">
+                  <RouterLink :to="{ name: 'sign-up' }">Sign up</RouterLink>
+              </li>
+          </ul>
+      </div>
       </Transition>
       <Transition name="light-box">
         <div v-if="mobileNav" class="bg-black/70 fixed top-0 left-0 w-full h-full z-20 xl:hidden" />
---
+++ src/components/NavLinks.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+defineProps({
+    mobileNav: null
+})
+</script>
+
+<template>
+    <ul class="my-10 text-blue-dark-grayish font-normal flex justify-evenly">
+        <li class="my-5 ml-5 hover:font-bold hover:text-blue-very-dark p-2">
+            <RouterLink :to="{ name: 'collections' }" @click="mobileNav = false">Collections</RouterLink>
+        </li>
+        <li class="my-5 ml-5 hover:font-bold hover:text-blue-very-dark p-2">
+            <RouterLink :to="{ name: 'men' }" @click="mobileNav = false">Men</RouterLink>
+        </li>
+        <li class="my-5 ml-5 hover:font-bold hover:text-blue-very-dark p-2">
+            <RouterLink :to="{ name: 'women' }" @click="mobileNav = false">Women</RouterLink>
+        </li>
+        <li class="my-5 ml-5 hover:font-bold hover:text-blue-very-dark p-2">
+            <RouterLink :to="{ name: 'about' }" @click="mobileNav = false">About</RouterLink >
+        </li>
+        <li class="my-5 ml-5 hover:font-bold hover:text-blue-very-dark p-2">
+            <RouterLink :to="{ name: 'contact' }" @click="mobileNav = false">Contact</RouterLink>
+        </li>
+        <li class="my-5 ml-5 hover:font-bold hover:text-blue-very-dark p-2">
+            <button>Sign in</button>
+        </li>
+        <li class="my-5 ml-5 hover:font-bold hover:text-blue-very-dark border border-blue-grayish hover:border-black rounded p-2">
+            <RouterLink :to="{ name: 'sign-up' }" @click="mobileNav = false">Sign up</RouterLink>
+        </li>
+    </ul>
+</template>
