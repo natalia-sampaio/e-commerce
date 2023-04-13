4b7c2499da6af2e5c177e8ea01218ba6a7c484d6
Natália Sampaio Rosa e Silva <natalia.srs@outlook.com>
2023-04-13 12:46:31 -0300

    :lipstick: changed responsive breakpoint

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@ export default {
         },
         checkScreen() {
           this.windowWidth = window.innerWidth;
-          if(this.windowWidth>= 1024) {
+          if(this.windowWidth>= 1280) {
             this.desktop = true;
             return
           }
@@ -41,9 +41,9 @@ export default {
 </script>
 
 <template>
-  <header class="flex items-center justify-between transition-all duration-500 lg:mx-40 lg:mb-20 lg:border-b-2 lg:border-blue-light-grayish">
+  <header class="flex items-center justify-between transition-all duration-500 xl:mx-40 xl:mb-20 xl:border-b-2 xl:border-blue-light-grayish">
     <div class="flex items-center transition-all duration-500">
-      <IconMenu class="m-3 transition-all duration-700 motion-reduce:transition-all lg:hidden" :class="{'rotate-180': mobileNav}" @click="toggleMobileNav" />
+      <IconMenu class="m-3 transition-all duration-700 motion-reduce:transition-all xl:hidden" :class="{'rotate-180': mobileNav}" @click="toggleMobileNav" />
       <RouterLink :to="{name: 'home'}"><IconLogo /></RouterLink>
       <div v-if="desktop" class="w-full">
         <ul class="mx-5 my-10 text-blue-dark-grayish font-normal flex justify-evenly">
@@ -73,39 +73,39 @@ export default {
       <Transition name="mobile-nav">
         <div v-show="mobileNav" class="
           bg-white 
-          fixed lg:relative 
+          fixed xl:relative 
           top-0 left-0 
-          h-full lg:h-auto 
-          w-2/3 lg:w-full 
-          z-30 lg:z-0 ">
-          <IconClose class="m-5 absolute right-0 transition-all duration-700 motion-reduce:transition-all lg:hidden" @click="toggleMobileNav" :class="{ '-rotate-180': !mobileNav }" />
-          <ul class="mx-5 my-10 lg:text-blue-dark-grayish lg:font-normal lg:flex lg:justify-evenly">
-            <li class="my-5 lg:ml-10">
+          h-full xl:h-auto 
+          w-2/3 xl:w-full 
+          z-30 xl:z-0 ">
+          <IconClose class="m-5 absolute right-0 transition-all duration-700 motion-reduce:transition-all xl:hidden" @click="toggleMobileNav" :class="{ '-rotate-180': !mobileNav }" />
+          <ul class="mx-5 my-10 xl:text-blue-dark-grayish xl:font-normal xl:flex xl:justify-evenly">
+            <li class="my-5 xl:ml-10">
               <RouterLink :to="{name: 'collections'}" @click="mobileNav = false">Collections</RouterLink>
             </li>
-            <li class="my-5 lg:ml-10">
+            <li class="my-5 xl:ml-10">
               <RouterLink :to="{name: 'men'}" @click="mobileNav = false">Men</RouterLink>
             </li>
-            <li class="my-5 lg:ml-10">
+            <li class="my-5 xl:ml-10">
               <RouterLink :to="{name: 'women'}" @click="mobileNav = false">Women</RouterLink>
             </li>
-            <li class="my-5 lg:ml-10">
+            <li class="my-5 xl:ml-10">
               <RouterLink :to="{name: 'about'}" @click="mobileNav = false">About</RouterLink >
             </li>
-            <li class="my-5 lg:ml-10">
+            <li class="my-5 xl:ml-10">
               <RouterLink :to="{name: 'contact'}" @click="mobileNav = false">Contact</RouterLink>
             </li>
-            <li class="my-5 lg:ml-10 border-b border-blue-dark-grayish pb-5">
+            <li class="my-5 xl:ml-10 border-b border-blue-dark-grayish pb-5">
               <button>Sign in</button>
             </li>
-            <li class="my-5 lg:ml-10">
+            <li class="my-5 xl:ml-10">
               <RouterLink :to="{name: 'sign-up'}">Sign up</RouterLink>
             </li>
           </ul>
         </div>
       </Transition>
       <Transition name="light-box">
-        <div v-if="mobileNav" class="bg-black/70 fixed top-0 left-0 w-full h-full z-20 lg:hidden" />
+        <div v-if="mobileNav" class="bg-black/70 fixed top-0 left-0 w-full h-full z-20 xl:hidden" />
       </Transition>
     </div>
     <div class="flex items-center">
@@ -113,7 +113,7 @@ export default {
         <span class="text-xs text-white rounded-3xl px-2 bg-orange-main absolute right-0 top-1">{{ cartStore.cartItems }}</span>
         <IconCart :fill="cartStore.toggleCart ? '#000000' : '#69707D'" class="m-3" @click="cartStore.toggleCart = !cartStore.toggleCart"/>
       </button>
-      <img src="./assets/images/image-avatar.png" alt="profile avatar" class="w-8 m-3 lg:w-16 lg:ml-10">
+      <img src="./assets/images/image-avatar.png" alt="profile avatar" class="w-8 m-3 xl:w-16 xl:ml-10">
     </div>
   </header>
   
