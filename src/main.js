a86c59e0f3b5a52d3b9081d0f6254091e44ffdac
Natália Sampaio Rosa e Silva <natalia.srs@outlook.com>
2023-04-18 22:02:11 -0300

    :heavy_plus_sign: build: firebase dependency

--- src/main.js
+++ src/main.js
@@ -6,6 +6,23 @@ import router from './router'
 
 import './assets/main.css'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB8N1ePfbQItb5k5m2B_7xkToAKfGGbWzM",
+  authDomain: "e-commerce-96ad7.firebaseapp.com",
+  projectId: "e-commerce-96ad7",
+  storageBucket: "e-commerce-96ad7.appspot.com",
+  messagingSenderId: "641521179925",
+  appId: "1:641521179925:web:c8653b24f4117d49bddd1c"
+};
+
+initializeApp(firebaseConfig);
+
 const app = createApp(App)
 
 app.use(createPinia())
